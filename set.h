088f816094043fe27d4dedcdfdef586e5a7d0c41
Nikolay Hizanishvili <nikolay.hizanishvili@gmail.com>
2021-08-24 00:54:01 +0300

    Add insertion functionality to multiset

--- set.h
+++ set.h
@@ -1,8 +1,85 @@
+#ifndef SET_H
+#define SET_H
+
 namespace mySTL
 {
     template <typename T>
 	class multiset
     {
-        
-    }
+        public:
+		multiset() {};
+
+        virtual void insert(T& key)
+		{
+			if (!root_) {
+				root_ = new node_();
+				root_->key = key;
+				return;
+			}
+
+			node_* parent = nullptr;
+			node_** node = &root_;
+			while (true) {
+				if (!(*node)) {
+					*node = new node_();
+					(*node)->key = key;
+					(*node)->parent = parent;
+					return;
+				}
+
+				if (key <= (*node)->key) {
+					parent = *node;
+					node = &((*node)->left_node);
+					continue;
+				}
+				parent = *node;
+				node = &((*node)->right_node);
+				continue;
+			}
+		}
+
+		virtual void insert(T&& key)
+		{
+			if (!root_) {
+				root_ = new node_();
+				root_->key = key;
+				return;
+			}
+
+			node_* parent = nullptr;
+			node_** node = &root_;
+			while (true) {
+				if (!(*node)) {
+					*node = new node_();
+					(*node)->key = key;
+					(*node)->parent = parent;
+					return;
+				}
+
+				if (key <= (*node)->key) {
+					parent = *node;
+					node = &((*node)->left_node);
+					continue;
+				}
+				parent = *node;
+				node = &((*node)->right_node);
+				continue;
+			}
+		}
+
+        protected:
+        struct node_
+		{
+			T key;
+
+			node_* left_node;
+			node_* right_node;
+			node_* parent;
+		};
+
+
+		node_* root_ = nullptr;
+    };
 }
+
+#endif
--- test.cpp
+++ test.cpp
@@ -1,4 +1,7 @@
+#include "set.h"
+
 int main()
 {
-    
+    mySTL::multiset<int>* mset = new mySTL::multiset<int>();
+    mset->insert(4);
 }
